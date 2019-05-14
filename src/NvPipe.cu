f086e7a015e2860f7c840a4e8705067ea3b7bc0b
Philip Andrew <philip368320@gmail.com>
2019-05-14 16:48:30 +0800

    For H264 encoding use the GUID

--- src/NvPipe.cu
+++ src/NvPipe.cu
@@ -663,6 +663,10 @@ private:
                 encodeConfig.rcParams.maxBitRate = encodeConfig.rcParams.averageBitRate;
                 encodeConfig.rcParams.vbvInitialDelay = encodeConfig.rcParams.vbvBufferSize;
             }
+            
+            // setting Profile baseline
+            if (this->codec == NVPIPE_H264)
+                encodeConfig.profileGUID = NV_ENC_H264_PROFILE_BASELINE_GUID;
 
             encoder->CreateEncoder(&initializeParams);
         }
